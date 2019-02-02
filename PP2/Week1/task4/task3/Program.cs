15748282609bf9902a89602f6e0ff56280335a0d
Eldar25 <eldar.sydykov.01@mail.ru>
2019-02-02 13:43:11 +0600

    PP2

---
+++ PP2/Week1/task4/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            int[] arr = new int[n];
+            for(int  i = 0; i < n; i++)
+            {
+               arr[i] = Console.ReadLine();
+            }
+        }
+    }
+}
