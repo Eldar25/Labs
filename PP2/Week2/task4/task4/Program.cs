9b6234b61a0d94333e34fb1e6a4f433a32acd5b9
Eldar25 <eldar.sydykov.01@mail.ru>
2019-02-09 13:05:48 +0600

    Week2

--- PP2/Week2/task4/task4/Program.cs
+++ PP2/Week2/task4/task4/Program.cs
@@ -11,25 +11,24 @@ namespace task4
     {
         static void Main(string[] args)
         {
-            string fname = Console.ReadLine();
-            string sdir1 = @"C:\Users\Пользователь\Desktop\Microsoft\Labs\PP2\Week2\task4\path";
-            string sdir2 = @"C:\Users\Пользователь\Desktop\Microsoft\Labs\PP2\Week2\task4\path1";
-            string dir1 = Path.Combine(sdir1, fname);
-            string dir2 = Path.Combine(sdir2, fname);
-            File.Create(dir1);
-            try
-            {
-                File.Copy(dir1, dir2);
-            }
-            catch (IOException copyError)
-            {
-                Console.WriteLine(copyError.Message);
-            }
-            foreach (var f in sdir1)
-            {
-                File.Delete(fname);
-            }
-            //File.Delete(dir1);
+            //DirectoryInfo di = new DirectoryInfo(@"C:\Users\Пользователь\Desktop\Microsoft\Labs\PP2\Week2\task4\path");
+            string di = (@"C:\Users\Пользователь\Desktop\Microsoft\Labs\PP2\Week2\task4\path\");
+            string path1 = @"C:\Users\Пользователь\Desktop\Microsoft\Labs\PP2\Week2\task4\path1";
+            string path = @"C:\Users\Пользователь\Desktop\Microsoft\Labs\PP2\Week2\task4\path";
+            string nameoft = Console.ReadLine();
+            string dir = Path.Combine(path, nameoft + ".txt");
+            string dir1 = Path.Combine(path1, nameoft + ".txt");
+            StreamWriter sw = File.CreateText(dir);
+            sw.Close();
+            Console.WriteLine("New file was created");
+            File.Copy(dir, dir1, true);
+            Console.WriteLine("File has been moved");
+            Console.WriteLine("Do you want to delete original?");
+            string s = Console.ReadLine();
+
+
+            File.Delete(di + nameoft + ".txt");
+            //File.Delete(@"C:\Users\Пользователь\Desktop\Microsoft\Labs\PP2\Week2\task4\path\papa1.txt");
         }
     }
 }
